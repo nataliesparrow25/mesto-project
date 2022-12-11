303d4cbb19b1267cc40375e8f2412cc113d68733
Natalia Vorobyova <nplyonk@gmail.com>
2022-12-11 20:32:19 +0500

    add: pop up appearance

---
+++ script.js
@@ -0,0 +1,26 @@
+let editButton = document.querySelector(".profile__edit-button");
+let userName = document.querySelector("h1");
+let inputName = document.getElementById("name");
+let popUp = document.getElementById("section-popup");
+let buttonClosePopUp = document.querySelector(".popup__button-close");
+let submitButton = document.querySelector(".popup__button");
+let userDescription = document.querySelector(".profile__user-description");
+let inputDescription = document.getElementById("description");
+
+function closePopUp() {
+  popUp.classList.remove("popup_opened");
+}
+
+editButton.addEventListener("click", () => {
+  popUp.classList.add("popup_opened");
+  inputName.value = userName.textContent;
+  inputDescription.value = userDescription.textContent;
+});
+
+submitButton.addEventListener("click", () => {
+  userName.textContent = inputName.value;
+  userDescription.textContent = inputDescription.value;
+  closePopUp();
+});
+
+buttonClosePopUp.addEventListener("click", closePopUp);
