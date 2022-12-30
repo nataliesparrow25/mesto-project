69e2300d3904449341bb57cab9391edde1f52603
Natalia Vorobyova <nplyonk@gmail.com>
2022-12-30 12:52:30 +0500

    hotfix: mandatory corrections

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const editButton = document.querySelector(".profile__edit-button");
 const userName = document.querySelector(".profile__user-name");
 const inputName = document.querySelector("#name");
-const popUpEditCard = document.querySelector("#section-popup-edit-profile");
 const userDescription = document.querySelector(".profile__user-description");
 const inputDescription = document.querySelector("#description");
 const titleCardInput = document.querySelector("#card-title");
 const imageCardInput = document.querySelector("#card-url");
 const addButton = document.querySelector(".profile__add-button");
-const popUpAddCard = document.querySelector("#section-popup-add-card");
 const cardsContainer = document.querySelector(".photo-grid__cards");
 const cardTemplate = document.querySelector("#grid-card-template").content;
 const cardElement = cardTemplate.querySelector(".photo-grid__card");
-const popUpPicImg = document.querySelector("#popup-pic-img");
 const popUpPicCaption = document.querySelector("#popup-pic-caption");
+const popUpPicImg = document.querySelector("#popup-pic-img");
+
+const popUpEditCard = document.querySelector("#section-popup-edit-profile");
+const popUpAddCard = document.querySelector("#section-popup-add-card");
+const popUpPicCard = document.querySelector("#section-popup-pic");
+
 const buttonClosePopUpEditCard = document.querySelector(
   "#close_pop-up_edit-profile"
 );
@@ -22,21 +25,21 @@ const buttonClosePopUpAddCard = document.querySelector(
 const buttonClosePopUpPic = document.querySelector("#close_pop-up_pic");
 
 
-function openPopUp(idElement) {
-  document.querySelector("#" + idElement).classList.add("popup_opened");
+function openPopUp(popup) {
+  popup.classList.add("popup_opened");
 }
 
-function closePopUp(idElement) {
-  document.querySelector("#" + idElement).classList.remove("popup_opened");
+function closePopUp(popup) {
+  popup.classList.remove("popup_opened");
 }
 
 buttonClosePopUpEditCard.addEventListener("click", (evt) => {
-  closePopUp("section-popup-edit-profile");
+  closePopUp(popUpEditCard);
 });
 
 
 editButton.addEventListener("click", () => {
-  openPopUp("section-popup-edit-profile");
+  openPopUp(popUpEditCard);
   inputName.value = userName.textContent;
   inputDescription.value = userDescription.textContent;
 });
@@ -47,7 +50,7 @@ document
     evt.preventDefault();
     userName.textContent = inputName.value;
     userDescription.textContent = inputDescription.value;
-    closePopUp("section-popup-edit-profile");
+    closePopUp(popUpEditCard);
   });
 
 
@@ -78,8 +81,9 @@ function createCard(imgSrc, titleCardValue) {
   newCard
     .querySelector(".photo-grid__image")
     .addEventListener("click", function (evt) {
-      openPopUp("section-popup-pic");
+      openPopUp(popUpPicCard);
       popUpPicImg.src = imgSrc;
+      popUpPicImg.alt = titleCardValue;
       popUpPicCaption.textContent = titleCardValue;
     });
   return newCard;
@@ -94,11 +98,11 @@ initialCards.forEach(function (element) {
 });
 
 buttonClosePopUpPic.addEventListener("click", (evt) => {
-  closePopUp("section-popup-pic");
+  closePopUp(popUpPicCard);
 });
 
 addButton.addEventListener("click", (evt) => {
-  openPopUp("section-popup-add-card");
+  openPopUp(popUpAddCard);
 });
 
 function resetInputValueAddCard() {
@@ -107,7 +111,7 @@ function resetInputValueAddCard() {
 }
 
 buttonClosePopUpAddCard.addEventListener("click", (evt) => {
-  closePopUp("section-popup-add-card");
+  closePopUp(popUpAddCard);
   resetInputValueAddCard();
 });
 
@@ -115,7 +119,7 @@ document
   .querySelector("#popup__form_add-card")
   .addEventListener("submit", (evt) => {
     evt.preventDefault();
-    closePopUp("section-popup-add-card");
+    closePopUp(popUpAddCard);
     renderCard(createCard(imageCardInput.value, titleCardInput.value));
     resetInputValueAddCard();
   });
